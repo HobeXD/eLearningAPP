30e0edf530f6988ace5fdc0a333f71d944e487b1
chihhsien.cheng <leisure.shadow@gmail.com>
2014-11-22 20:12:45 +0800

    Add question selection and game over.

--- Fish/level1.lua
+++ Fish/level1.lua
@@ -11,10 +11,31 @@ local scene = composer.newScene()
 local physics = require "physics"
 physics.start(); physics.pause()
 
+local widget = require "widget"
+
 --------------------------------------------
 
 -- forward declarations and other locals
 local screenW, screenH, halfW = display.contentWidth, display.contentHeight, display.contentWidth*0.5
+local wave
+
+local function onAnswerBtn1Release()
+	local yNew = wave.y -10
+	if ( yNew < wave.height / 2 ) then
+		wave.y = wave.height / 2
+	else
+		wave.y = yNew
+	end
+end
+
+local function onAnswerBtn2Release()
+	local yNew = wave.y + 10
+	if ( yNew > wave.height * 1.5 ) then
+		composer.gotoScene( "game_over", "fade", 500 )
+	else
+		wave.y = yNew + 10
+	end
+end
 
 function scene:create( event )
 
@@ -26,10 +47,10 @@ function scene:create( event )
 	local sceneGroup = self.view
 
 
-	local wave = display.newImageRect( sceneGroup, "wave.png", display.contentWidth, display.contentHeight)
+	wave = display.newImageRect( sceneGroup, "wave.png", display.contentWidth, display.contentHeight)
     wave.x, wave.y = display.contentWidth/2, display.contentHeight - wave.contentHeight/2
     wave.ydir = 1
-	wave.yspeed = 2
+	wave.yspeed = 1
 
 	local fish_left_img = { type="image", filename="fish_left.png" }
 	local fish_right_img = { type="image", filename="fish_right.png" }
@@ -39,46 +60,37 @@ function scene:create( event )
 	fish.fill = fish_right_img
     -- fish.x, fish.y = display.contentWidth/2, display.contentHeight/2
     fish.xdir = 1
-	fish.xspeed = 2
+	fish.xspeed = 1.5
     fish.ydir = 1
 	fish.yspeed = 2
+ 
+	local question = display.newText( {parent=sceneGroup, text="Color", x=display.contentWidth/2, y=50, font=native.systemFont, fontSize=30} )
 
-	print(display.contentWidth)
-	print(display.contentHeight)
-	print(wave.contentWidth)
-	print(wave.contentHeight)
-
-	local circle = display.newCircle( display.contentWidth*0.5, 0, 10 );
-	circle:setFillColor( 255, 255, 255, 255 );
-
-	local function newBall( params )
-		local xpos = display.contentWidth*0.5
-		local ypos = display.contentHeight*0.5
-		local circle = display.newCircle( xpos, ypos, params.radius );
-		circle:setFillColor( params.r, params.g, params.b, 255 );
-		circle.xdir = params.xdir
-		circle.ydir = params.ydir
-		circle.xspeed = params.xspeed
-		circle.yspeed = params.yspeed
-		circle.radius = params.radius
-
-		return circle
-	end
-
-	local params = {
-		{ radius=20, xdir=1, ydir=1, xspeed=2.8, yspeed=6.1, r=255, g=0, b=0 },
-		{ radius=12, xdir=1, ydir=1, xspeed=3.8, yspeed=4.2, r=255, g=255, b=0 },
-		{ radius=15, xdir=1, ydir=-1, xspeed=5.8, yspeed=5.5, r=255, g=0, b=255 },
-	--	newBall{ radius=10, xdir=-1, ydir=1, xspeed=3.8, yspeed=1.2 }
+	answerBtn1 = widget.newButton{
+		label="顏色",
+		labelColor = { default={255}, over={128} },
+		default="button.png",
+		over="button-over.png",
+		width=154, height=40,
+		onRelease = onAnswerBtn1Release	-- event listener function
 	}
+	answerBtn1.x = display.contentWidth/4
+	answerBtn1.y = 80
 
-	local collection = {}
+	answerBtn2 = widget.newButton{
+		label="大便",
+		labelColor = { default={255}, over={128} },
+		default="button.png",
+		over="button-over.png",
+		width=154, height=40,
+		onRelease = onAnswerBtn2Release	-- event listener function
+	}
+	answerBtn2.x = display.contentWidth*3/4
+	answerBtn2.y = 80
 
-	-- Iterate through params array and add new balls into an array
-	for _,item in ipairs( params ) do
-		local ball = newBall( item )
-		collection[ #collection + 1 ] = ball
-	end
+
+	sceneGroup:insert( answerBtn1 )
+	sceneGroup:insert( answerBtn2 )
 
 	-- Get current edges of visible screen (accounting for the areas cropped by "zoomEven" scaling mode in config.lua)
 	local screenTop = display.screenOriginY
@@ -86,38 +98,19 @@ function scene:create( event )
 	local screenLeft = display.screenOriginX
 	local screenRight = display.viewableContentWidth + display.screenOriginX
 
-	function collection:enterFrame( event )
-		for _,ball in ipairs( collection ) do
-			local dx = ( ball.xspeed * ball.xdir );
-			local dy = ( ball.yspeed * ball.ydir );
-			local xNew, yNew = ball.x + dx, ball.y + dy
-
-			local radius = ball.radius
-			if ( xNew > screenRight - radius or xNew < screenLeft + radius ) then
-				ball.xdir = -ball.xdir
-			end
-			if ( yNew > screenBottom - radius or yNew < screenTop + radius ) then
-				ball.ydir = -ball.ydir
-			end
-
-			ball:translate( dx, dy )
-		end
-	end
-
 	function wave:enterFrame( event )
-		local dy = ( wave.yspeed * wave.ydir );
-		local yNew = wave.y + dy
+		local yNew = wave.y + wave.yspeed
 
-		if ( yNew > screenBottom + wave.height / 2 - 4 * fish.contentHeight or yNew < screenBottom - wave.height / 2) then
-			wave.ydir = -wave.ydir
+		if ( yNew > wave.height * 1.5) then
+			composer.gotoScene( "game_over", "fade", 500 )
 		end
 
-		wave:translate( 0, dy )
+		wave:translate( 0, wave.yspeed )
 	end
 
 	function fish:enterFrame( event )
-		local dy = ( fish.yspeed * fish.ydir );
-		local dx = ( fish.xspeed * fish.xdir );
+		local dy = fish.yspeed * fish.ydir
+		local dx = fish.xspeed * fish.xdir 
 		local yNew = fish.y + dy
 		local xNew = fish.x + dx
 
@@ -136,7 +129,6 @@ function scene:create( event )
 		fish:translate( dx, dy )
 	end
 
-	Runtime:addEventListener( "enterFrame", collection );
 	Runtime:addEventListener( "enterFrame", wave );
 	Runtime:addEventListener( "enterFrame", fish );
 end
