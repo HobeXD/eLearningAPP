dc4b1d0ed5affab1693c8b7265a7dadf7efa129b
Claire <clairechang@clairedeMacBook-Pro.local>
2014-11-22 20:27:07 +0800

    add 20 question.

--- sheep/level1.lua
+++ sheep/level1.lua
@@ -7,20 +7,36 @@
 local composer = require( "composer" )
 local scene = composer.newScene()
 local widget = require "widget"
-local physics = require "physics"
-local tagsGroup = display.newGroup()
-local localGroup = display.newGroup()
+local math = require "math"
+local initGroup = display.newGroup()
+--local localGroup = display.newGroup()
 
 --------------------------------------------
 
 -- forward declarations and other locals
-local screenW, screenH, halfW = display.contentWidth, display.contentHeight, display.contentWidth*0.5
+local screenW, screenH, halfW , halfH= display.contentWidth, display.contentHeight, display.contentWidth*0.5, display.contentHeight*0.5
 local tags = {}
+local questionNum = 0
+local chinese
 
-local function onTagsRelease()
-	
+local function nextQuestion( event )
+	local tmp = math.random(3)
+	questionNum = math.random(vocNum - 1)
+	chinese.text = question[questionNum]
+	for i = 0, 2 do
+		tags[(tmp + i) % 3]:setLabel(answer[(questionNum + i * math.random(vocNum / 2 - 1)) % vocNum])
+	end
+end
 
-	
+local function onTagsRelease( event )
+	if( event.target:getLabel() == answer[questionNum] )then
+		score = score + 10
+		showScore.text = score
+	else
+		score = score - 10
+		showScore.text = score
+	end
+	nextQuestion()
 	return true	-- indicates successful touch
 end
 
@@ -33,29 +49,11 @@ function scene:create( event )
 	background.anchorY = 0
 	background.x, background.y = 0, 0
 
-	for i = 1, 3 do
-		tags[i] = widget.newButton{
-			label = "apple",
-			labelColor = { default={000}, over={159/255, 80/255, 0} },
-			fontSize = "28",
-			defaultFile="./level1/tag.png",
-			overFile="./level1/tag-over.png",
-			width=100, height=80,
-			onRelease = onTagRelease,
-			--isEnabled = false
-		}
-		tags[i].x = display.contentWidth*(0.04 + 0.2 * (i - 1)) 
-		tags[i].y = display.contentHeight*0.85
-	end
-	
+	ready_go()
+	timer.performWithDelay( 3700, init )
+
 	-- all display objects must be inserted into group
 	sceneGroup:insert( background )
-	tagsGroup.alpha = 0
-	tagsGroup:insert( tags[1] )
-	tagsGroup:insert( tags[2] )
-	tagsGroup:insert( tags[3] )
-	transition.to( tagsGroup, { time = 500, delay = 3700, alpha = 1 } )
-	--sceneGroup:insert( tags[1], tags[2], tags[3] )
 end
 
 
@@ -70,13 +68,11 @@ function scene:show( event )
 		-- 
 		-- INSERT code here to make the scene come alive
 		-- e.g. start timers, begin animation, play audio, etc.
-		physics.start()
 	end
 end
 
 function scene:hide( event )
 	local sceneGroup = self.view
-	
 	local phase = event.phase
 	
 	if event.phase == "will" then
@@ -84,11 +80,9 @@ function scene:hide( event )
 		--
 		-- INSERT code here to pause the scene
 		-- e.g. stop timers, stop animation, unload sounds, etc.)
-		physics.stop()
 	elseif phase == "did" then
 		-- Called when the scene is now off screen
 	end	
-	
 end
 
 function scene:destroy( event )
@@ -98,37 +92,70 @@ function scene:destroy( event )
 	-- INSERT code here to cleanup the scene
 	-- e.g. remove display objects, remove touch listeners, save state, etc.
 	local sceneGroup = self.view
-	
-	package.loaded[physics] = nil
-	physics = nil
 end
 
-function showTags( event )
-
-	
+function sheep( event )
+	chinese = display.newText(question[questionNum], halfW, halfH, native.systemFont, 30, right)
+	showScore:setFillColor( 1, 211/255, 0 )
 end
 
---ready? & go!
-local ready = display.newImage("./level1/ready.png")
-ready.xScale = 0.05
-ready.yScale = 0.05
-localGroup:insert(ready)
-ready.x = -150
-ready.y = screenH/2
-transition.to( ready, { time = 300, delay = 800, x = halfW, xScale = 0.2, yScale = 0.2} )
-transition.to( ready, { time = 300, delay = 2100, x = screenW + 150, xScale = 0.05, yScale = 0.05} )
+function init( event )
+	initGroup.alpha = 0
 
-local go = display.newImage("./level1/go.png")
-go.xScale = 0.1
-go.yScale = 0.1
-localGroup:insert(go)
-go.x = -150
-go.y = screenH/2
-transition.to( go, { time = 300, delay = 2400, x = halfW, xScale = 0.2, yScale = 0.2} )
-transition.to( go, { time = 300, delay = 3700, x = screenW + 150, xScale = 0.05, yScale = 0.05} )
+	local tmp = math.random(3)
+	questionNum = math.random(vocNum - 1)
+	for i = 0, 2 do
+		tags[(tmp + i) % 3] = widget.newButton{
+			label = answer[(questionNum + i * math.random(vocNum / 2 - 1)) % vocNum],
+			labelColor = { default={000}, over={159/255, 80/255, 0} },
+			fontSize = "18",
+			defaultFile="./level1/tag.png",
+			overFile="./level1/tag-over.png",
+			width=100, height=80,
+			onRelease = onTagsRelease
+		}
+		tags[(tmp + i) % 3].x = display.contentWidth*(0.04 + 0.2 * i) 
+		tags[(tmp + i) % 3].y = display.contentHeight*0.85
+	end
 
---show three tags
---transition.to( tagsGroup, { time = 300, delay = 3700, isVisible = true, } )
+	local score_logo = display.newImage( "./level1/score.png")
+	score_logo.x = screenH * 0.55
+	score_logo.y = screenW * 0.05
+	score_logo.xScale = 0.1
+	score_logo.yScale = 0.1
+
+	score = 0
+	showScore = display.newText(score, 280, 20, native.systemFont, 30, right)
+	showScore:setFillColor( 1, 211/255, 0 )
+
+	initGroup:insert( tags[0] )
+	initGroup:insert( tags[1] )
+	initGroup:insert( tags[2] )
+	initGroup:insert( score_logo )
+
+	transition.to(initGroup, {time = 300, alpha = 1})
+	sheep()
+end
+
+function ready_go( event )
+	local ready = display.newImage("./level1/ready.png")
+	ready.xScale = 0.05
+	ready.yScale = 0.05
+	initGroup:insert(ready)
+	ready.x = -150
+	ready.y = screenH/2
+	transition.to( ready, { time = 300, delay = 800, x = halfW, xScale = 0.2, yScale = 0.2} )
+	transition.to( ready, { time = 300, delay = 2100, x = screenW + 150, xScale = 0.05, yScale = 0.05} )
+
+	local go = display.newImage("./level1/go.png")
+	go.xScale = 0.1
+	go.yScale = 0.1
+	initGroup:insert(go)
+	go.x = -150
+	go.y = screenH/2
+	transition.to( go, { time = 300, delay = 2400, x = halfW, xScale = 0.2, yScale = 0.2} )
+	transition.to( go, { time = 300, delay = 3700, x = screenW + 150, xScale = 0.05, yScale = 0.05} )
+end
 
 ---------------------------------------------------------------------------------
 
--- sheep/main.lua
+++ sheep/main.lua
@@ -12,3 +12,51 @@ local composer = require "composer"
 
 -- load menu screen
 composer.gotoScene( "menu" )
+
+vocNum = 20
+
+question = {
+	[0] = "禮物",
+	[1] = "尺",
+	[2] = "作業本",
+	[3] = "教室",
+	[4] = "操場",
+	[5] = "圖書館",
+	[6] = "班級",
+	[7] = "黑板",
+	[8] = "書",
+	[9] = "粉筆",
+	[10] = "字典",
+	[11] = "信",
+	[12] = "地圖",
+	[13] = "筆記本",
+	[14] = "橡皮擦",
+	[15] = "頁",
+	[16] = "紙",
+	[17] = "筆",
+	[18] = "鉛筆",
+	[19] = "圖片"
+}
+
+answer = {
+	[0] = "present",
+	[1] = "ruler",
+	[2] = "workbook",
+	[3] = "classroom",
+	[4] = "playground",
+	[5] = "library",
+	[6] = "class",
+	[7] = "blackboard",
+	[8] = "book",
+	[9] = "chalk",
+	[10] = "dictionary",
+	[11] = "letter",
+	[12] = "map",
+	[13] = "notebook",
+	[14] = "eraser",
+	[15] = "page",
+	[16] = "paper",
+	[17] = "pen",
+	[18] = "pencil",
+	[19] = "picture"
+}
