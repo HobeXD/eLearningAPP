3dec5e67e6116b69a9e12cbfb97d82ba9d898153
chihhsien.cheng <leisure.shadow@gmail.com>
2014-12-07 22:36:08 +0800

    add random selecting question and score display

--- Fish/level1.lua
+++ Fish/level1.lua
@@ -17,10 +17,65 @@ local widget = require "widget"
 
 -- forward declarations and other locals
 local screenW, screenH, halfW = display.contentWidth, display.contentHeight, display.contentWidth*0.5
-local wave
+local wave, fish
+local fish_left_img, fish_right_img
+local question, answerBtn1, answerBtn2, correct_ans
+local score, score_word, score_text
+local en = {}
+local ch = {}
 
-local function onAnswerBtn1Release()
-	local yNew = wave.y -10
+math.randomseed(os.time())
+
+function string:split( inSplitPattern, outResults )
+
+   if not outResults then
+      outResults = { }
+   end
+   local theStart = 1
+   local theSplitStart, theSplitEnd = string.find( self, inSplitPattern, theStart )
+   while theSplitStart do
+      table.insert( outResults, string.sub( self, theStart, theSplitStart-1 ) )
+      theStart = theSplitEnd + 1
+      theSplitStart, theSplitEnd = string.find( self, inSplitPattern, theStart )
+   end
+   table.insert( outResults, string.sub( self, theStart ) )
+   return outResults
+end
+
+local function getWrongAnswerIndex( index)
+	local wrong_ans_index = math.random(table.getn(en))
+	while wrong_ans_index == index do
+		wrong_ans_index = math.random(table.getn(en))
+	end
+	return wrong_ans_index
+end
+
+local function setQuestion()
+	local index = math.random(table.getn(en))
+	question.text = en[index]
+	correct_ans = math.random(2)
+	if correct_ans == 1 then
+		answerBtn1:setLabel(ch[index])
+		local wrong_ans_index = getWrongAnswerIndex(index)
+		answerBtn2:setLabel(ch[wrong_ans_index])
+	else
+		answerBtn2:setLabel(ch[index])
+		local wrong_ans_index = getWrongAnswerIndex(index)
+		answerBtn1:setLabel(ch[wrong_ans_index])
+	end
+end
+
+local function decreaseWaveHeight()
+	local yNew = wave.y + 10
+	if ( yNew > wave.height * 1.5  - 2.5 * fish.height) then
+		composer.gotoScene( "game_over", "fade", 500 )
+	else
+		wave.y = yNew + 10
+	end
+end
+
+local function increaseWaveHeight()
+	local yNew = wave.y - 30
 	if ( yNew < wave.height / 2 ) then
 		wave.y = wave.height / 2
 	else
@@ -28,13 +83,26 @@ local function onAnswerBtn1Release()
 	end
 end
 
-local function onAnswerBtn2Release()
-	local yNew = wave.y + 10
-	if ( yNew > wave.height * 1.5 ) then
-		composer.gotoScene( "game_over", "fade", 500 )
+local function onAnswerBtn1Release()
+	if correct_ans == 1 then
+		increaseWaveHeight()
+		score = math.floor(score + (wave.height * 1.5  - 2.5 * fish.height - wave.y)/10)
+		score_text.text = score
 	else
-		wave.y = yNew + 10
+		decreaseWaveHeight()
 	end
+	setQuestion()
+end
+
+local function onAnswerBtn2Release()
+	if correct_ans == 2 then
+		increaseWaveHeight()
+		score = math.floor(score + (wave.height * 1.5  - 2.5 * fish.height - wave.y)/10)
+		score_text.text = score
+	else
+		decreaseWaveHeight()
+	end
+	setQuestion()
 end
 
 function scene:create( event )
@@ -52,11 +120,11 @@ function scene:create( event )
     wave.ydir = 1
 	wave.yspeed = 1
 
-	local fish_left_img = { type="image", filename="fish_left.png" }
-	local fish_right_img = { type="image", filename="fish_right.png" }
+	fish_left_img = { type="image", filename="fish_left.png" }
+	fish_right_img = { type="image", filename="fish_right.png" }
 
 	-- local fish = display.newImageRect( sceneGroup, "fish_left.png", 90, 45)
-	local fish = display.newRect( sceneGroup, display.contentWidth/2, display.contentHeight/2, 90, 45)
+	fish = display.newRect( sceneGroup, display.contentWidth/2, display.contentHeight/2, 90, 45)
 	fish.fill = fish_right_img
     -- fish.x, fish.y = display.contentWidth/2, display.contentHeight/2
     fish.xdir = 1
@@ -64,10 +132,13 @@ function scene:create( event )
     fish.ydir = 1
 	fish.yspeed = 2
  
-	local question = display.newText( {parent=sceneGroup, text="Color", x=display.contentWidth/2, y=50, font=native.systemFont, fontSize=30} )
+	question = display.newText( {parent=sceneGroup, text="Color", x=display.contentWidth/2, y=70, font=native.systemFont, fontSize=30} )
+	score_word = display.newText( {parent=sceneGroup, text="Score: ", font=native.systemFont, fontSize=20} )
+	score_word.x, score_word.y = score_word.width/2 + 10, score_word.height/2 + 10
+	score_text = display.newText( {parent=sceneGroup, text="0", font=native.systemFont, fontSize=20} )
+	score_text.x, score_text.y = score_word.x + score_word.width/2 + score_text.width/2 + 10, score_word.height
 
 	answerBtn1 = widget.newButton{
-		label="顏色",
 		labelColor = { default={255}, over={128} },
 		default="button.png",
 		over="button-over.png",
@@ -75,10 +146,9 @@ function scene:create( event )
 		onRelease = onAnswerBtn1Release	-- event listener function
 	}
 	answerBtn1.x = display.contentWidth/4
-	answerBtn1.y = 80
+	answerBtn1.y = 100
 
 	answerBtn2 = widget.newButton{
-		label="大便",
 		labelColor = { default={255}, over={128} },
 		default="button.png",
 		over="button-over.png",
@@ -86,51 +156,25 @@ function scene:create( event )
 		onRelease = onAnswerBtn2Release	-- event listener function
 	}
 	answerBtn2.x = display.contentWidth*3/4
-	answerBtn2.y = 80
+	answerBtn2.y = 100
 
 
 	sceneGroup:insert( answerBtn1 )
 	sceneGroup:insert( answerBtn2 )
 
-	-- Get current edges of visible screen (accounting for the areas cropped by "zoomEven" scaling mode in config.lua)
-	local screenTop = display.screenOriginY
-	local screenBottom = display.viewableContentHeight + display.screenOriginY
-	local screenLeft = display.screenOriginX
-	local screenRight = display.viewableContentWidth + display.screenOriginX
+	local path = system.pathForFile( "level1 words.csv" )
+	local file = io.open( path, "r" )
 
-	function wave:enterFrame( event )
-		local yNew = wave.y + wave.yspeed
-
-		if ( yNew > wave.height * 1.5) then
-			composer.gotoScene( "game_over", "fade", 500 )
-		end
-
-		wave:translate( 0, wave.yspeed )
+	for line in file:lines() do
+		-- if line == nil then break end
+	    local tmp = line:split(",")
+	    table.insert(en, tmp[1])
+	    table.insert(ch, tmp[2])
 	end
 
-	function fish:enterFrame( event )
-		local dy = fish.yspeed * fish.ydir
-		local dx = fish.xspeed * fish.xdir 
-		local yNew = fish.y + dy
-		local xNew = fish.x + dx
+	io.close( file )
+	file = nil
 
-		if ( xNew > screenRight - fish.contentWidth/2 or xNew < screenLeft + fish.contentWidth/2 ) then
-			fish.xdir = -fish.xdir
-			if (fish.xdir < 0) then
-				fish.fill = fish_left_img
-			else
-				fish.fill = fish_right_img
-			end
-		end		
-		if ( fish.ydir >= 0 and yNew > screenBottom - fish.contentHeight/2 or fish.ydir < 0 and yNew < wave.y - wave.contentHeight / 2 + 2 * fish.contentHeight ) then
-			fish.ydir = -fish.ydir
-		end
-
-		fish:translate( dx, dy )
-	end
-
-	Runtime:addEventListener( "enterFrame", wave );
-	Runtime:addEventListener( "enterFrame", fish );
 end
 
 
@@ -140,12 +184,57 @@ function scene:show( event )
 	
 	if phase == "will" then
 		-- Called when the scene is still off screen and is about to move on screen
+		wave.x, wave.y = display.contentWidth/2, display.contentHeight - wave.contentHeight/2
+		fish.x, fish.y = display.contentWidth/2, display.contentHeight/2
+		setQuestion()
+
 	elseif phase == "did" then
 		-- Called when the scene is now on screen
 		-- 
 		-- INSERT code here to make the scene come alive
 		-- e.g. start timers, begin animation, play audio, etc.   
 		-- physics.start()
+		local screenTop = display.screenOriginY
+		local screenBottom = display.viewableContentHeight + display.screenOriginY
+		local screenLeft = display.screenOriginX
+		local screenRight = display.viewableContentWidth + display.screenOriginX
+
+		function wave:enterFrame( event )
+			local yNew = wave.y + wave.yspeed
+
+			if ( yNew > wave.height * 1.5 - 2.5 * fish.height) then
+				composer.gotoScene( "game_over", "fade", 500 )
+			end
+
+			wave:translate( 0, wave.yspeed )
+		end
+
+		function fish:enterFrame( event )
+			local dy = fish.yspeed * fish.ydir
+			local dx = fish.xspeed * fish.xdir 
+			local yNew = fish.y + dy
+			local xNew = fish.x + dx
+
+			if ( xNew > screenRight - fish.contentWidth/2 or xNew < screenLeft + fish.contentWidth/2 ) then
+				fish.xdir = -fish.xdir
+				if (fish.xdir < 0) then
+					fish.fill = fish_left_img
+				else
+					fish.fill = fish_right_img
+				end
+			end		
+			if ( fish.ydir >= 0 and yNew > screenBottom - fish.contentHeight/2 or fish.ydir < 0 and yNew < wave.y - wave.contentHeight / 2 + 2 * fish.contentHeight ) then
+				fish.ydir = -fish.ydir
+			end
+
+			fish:translate( dx, dy )
+		end
+
+		Runtime:addEventListener( "enterFrame", wave )
+		Runtime:addEventListener( "enterFrame", fish )
+
+		score = 0
+		score_text.text = score
 	end
 end
 
