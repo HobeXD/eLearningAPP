cf478e9fe51694feb30dc516c397941415d4e10a
chihhsien.cheng <leisure.shadow@gmail.com>
2015-01-24 15:17:57 +0800

    refactor level_selection and menu

--- Fish/level_selection.lua
+++ Fish/level_selection.lua
@@ -1,209 +1,48 @@
------------------------------------------------------------------------------------------
---
--- menu.lua
---
------------------------------------------------------------------------------------------
-
 local composer = require( "composer" )
-local scene = composer.newScene()
-
--- include Corona's "widget" library
 local widget = require "widget"
-
---------------------------------------------
-
-
--- 'onRelease' event listener for playBtn
-local function onSchoolBtnRelease()
-	
-	-- go to level1.lua scene
-	local options = {
-	    effect = "fade",
-	    time = 500,
-	    params = {word = "school"}
-	}
-	composer.gotoScene( "game", options )
-	
-	return true	-- indicates successful touch
-end
-
-local function onPlaceLocationBtnRelease()
-	
-	-- go to level1.lua scene
-	local options = {
-	    effect = "fade",
-	    time = 500,
-	    params = {word = "place_location"}
-	}
-	composer.gotoScene( "game", options )
-	
-	return true	-- indicates successful touch
-end
-
-local function onTransportationBtnRelease()
-	
-	-- go to level1.lua scene
-	local options = {
-	    effect = "fade",
-	    time = 500,
-	    params = {word = "transportation"}
-	}
-	composer.gotoScene( "game", options )
-	
-	return true	-- indicates successful touch
-end
-
-local function onPersonalCharacteristicsBtnRelease()
-	
-	-- go to level1.lua scene
-	local options = {
-	    effect = "fade",
-	    time = 500,
-	    params = {word = "personal_characteristics"}
-	}
-	composer.gotoScene( "game", options )
-	
-	return true	-- indicates successful touch
-end
-
-local function onTimeBtnRelease()
-	
-	-- go to level1.lua scene
-	local options = {
-	    effect = "fade",
-	    time = 500,
-	    params = {word = "time"}
-	}
-	composer.gotoScene( "game", options )
-	
-	return true	-- indicates successful touch
-end
+local scene = composer.newScene()
 
 function scene:create( event )
 	local sceneGroup = self.view
-
-	-- Called when the scene's view does not exist.
-	-- 
-	-- INSERT code here to initialize the scene
-	-- e.g. add display objects to 'sceneGroup', add touch listeners, etc.
-
-	-- display a background image
 	local background = display.newImageRect( "background.jpg", display.contentWidth, display.contentHeight )
-	background.anchorX = 0
-	background.anchorY = 0
-	background.x, background.y = 0, 0
-	
-	-- create a widget button (which will loads level1.lua on release)
-	schoolBtn = widget.newButton{
-		label="School",
-		labelColor = { default={255}, over={128} },
-		defaultFile="button.png",
-		overFile="button-over.png",
-		width=220, height=40,
-		onRelease = onSchoolBtnRelease	-- event listener function
-	}
-	schoolBtn.x = display.contentWidth*0.5
-	schoolBtn.y = display.contentHeight - 400
-
-	place_locationBtn = widget.newButton{
-		label="Place & Location",
-		labelColor = { default={255}, over={128} },
-		defaultFile="button.png",
-		overFile="button-over.png",
-		width=220, height=40,
-		onRelease = onPlaceLocationBtnRelease	-- event listener function
-	}
-	place_locationBtn.x = display.contentWidth*0.5
-	place_locationBtn.y = display.contentHeight - 320
-
-	transportationBtn = widget.newButton{
-		label="Transportation",
-		labelColor = { default={255}, over={128} },
-		defaultFile="button.png",
-		overFile="button-over.png",
-		width=220, height=40,
-		onRelease = onTransportationBtnRelease	-- event listener function
-	}
-	transportationBtn.x = display.contentWidth*0.5
-	transportationBtn.y = display.contentHeight - 240
-
-	personal_characteristicsBtn = widget.newButton{
-		label="Personal & Characteristics",
-		labelColor = { default={255}, over={128} },
-		defaultFile="button.png",
-		overFile="button-over.png",
-		width=220, height=40,
-		onRelease = onPersonalCharacteristicsBtnRelease	-- event listener function
-	}
-	personal_characteristicsBtn.x = display.contentWidth*0.5
-	personal_characteristicsBtn.y = display.contentHeight - 160
-
-	timeBtn = widget.newButton{
-		label="Time",
-		labelColor = { default={255}, over={128} },
-		defaultFile="button.png",
-		overFile="button-over.png",
-		width=220, height=40,
-		onRelease = onTimeBtnRelease	-- event listener function
-	}
-	timeBtn.x = display.contentWidth*0.5
-	timeBtn.y = display.contentHeight - 80
-	
-	-- all display objects must be inserted into group
+	background.x, background.y = display.contentWidth / 2, display.contentHeight / 2
 	sceneGroup:insert( background )
-	sceneGroup:insert( schoolBtn )
-	sceneGroup:insert( place_locationBtn )
-	sceneGroup:insert( transportationBtn )
-	sceneGroup:insert( personal_characteristicsBtn )
-	sceneGroup:insert( timeBtn )
-end
 
-function scene:show( event )
-	local sceneGroup = self.view
-	local phase = event.phase
-	
-	if phase == "will" then
-		-- Called when the scene is still off screen and is about to move on screen
-	elseif phase == "did" then
-		-- Called when the scene is now on screen
-		-- 
-		-- INSERT code here to make the scene come alive
-		-- e.g. start timers, begin animation, play audio, etc.
-	end	
-end
+	theme = {"School", "Place & Location", "Transportation", "Personal & Characteristics", "Time"}
+	level_name = {"school", "place_location", "transportation", "personal_characteristics", "time"}
 
-function scene:hide( event )
-	local sceneGroup = self.view
-	local phase = event.phase
-	
-	if event.phase == "will" then
-		-- Called when the scene is on screen and is about to move off screen
-		--
-		-- INSERT code here to pause the scene
-		-- e.g. stop timers, stop animation, unload sounds, etc.)
-	elseif phase == "did" then
-		-- Called when the scene is now off screen
-	end	
+	local function onBtnRelease(level)
+		local options = {
+		    effect = "fade",
+		    time = 500,
+		    params = {word = level}
+		}
+		composer.gotoScene( "game", options )
+		return true	-- indicates successful touch
+	end
+
+	for i=1, #theme do 
+		local btn = widget.newButton{
+			label = theme[i],
+			labelColor = { default={255}, over={128} },
+			defaultFile = "button.png",
+			overFile = "button-over.png",
+			width = 220, height = 40,
+			x = display.contentWidth/2,
+			y = display.contentHeight - 480 + 80*i,
+			onRelease = function() return onBtnRelease(level_name[i]) end
+		}
+		sceneGroup:insert( btn )
+	end
 end
 
 function scene:destroy( event )
 	local sceneGroup = self.view
-	
-	-- Called prior to the removal of scene's "view" (sceneGroup)
-	-- 
-	-- INSERT code here to cleanup the scene
-	-- e.g. remove display objects, remove touch listeners, save state, etc.
-
+	for i=1, #sceneGroup do 
+		sceneGroup[i]:removeSelf()
+	end
 end
 
----------------------------------------------------------------------------------
-
--- Listener setup
 scene:addEventListener( "create", scene )
-scene:addEventListener( "show", scene )
-scene:addEventListener( "hide", scene )
 scene:addEventListener( "destroy", scene )
-
------------------------------------------------------------------------------------------
-
 return scene
--- Fish/menu.lua
+++ Fish/menu.lua
@@ -1,113 +1,43 @@
------------------------------------------------------------------------------------------
---
--- menu.lua
---
------------------------------------------------------------------------------------------
-
 local composer = require( "composer" )
-local scene = composer.newScene()
-
--- include Corona's "widget" library
 local widget = require "widget"
-
---------------------------------------------
-
--- forward declarations and other locals
-local playBtn
-
--- 'onRelease' event listener for playBtn
-local function onPlayBtnRelease()
-	
-	composer.gotoScene( "level_selection", "fade", 500 )
-	
-	return true	-- indicates successful touch
-end
+local scene = composer.newScene()
 
 function scene:create( event )
 	local sceneGroup = self.view
+	local background = display.newImageRect(sceneGroup, "background.jpg", display.contentWidth, display.contentHeight )
+	background.x, background.y = display.contentWidth / 2, display.contentHeight / 2
 
-	-- Called when the scene's view does not exist.
-	-- 
-	-- INSERT code here to initialize the scene
-	-- e.g. add display objects to 'sceneGroup', add touch listeners, etc.
+	local title = display.newText(sceneGroup, "Spelling Fish", display.contentWidth / 2, display.contentHeight / 4, native.systemFontBold, 40)
+	title:setFillColor( 0.2, 0.2, 0.5 )
 
-	-- display a background image
-	local background = display.newImageRect( "background.jpg", display.contentWidth, display.contentHeight )
-	background.anchorX = 0
-	background.anchorY = 0
-	background.x, background.y = 0, 0
-	
-	-- create/position logo/title image on upper-half of the screen
-	local title = display.newText("Spelling Fish", display.contentWidth / 2, display.contentHeight / 2, native.systemFontBold, 40)
-	
-	-- create a widget button (which will loads level1.lua on release)
-	playBtn = widget.newButton{
-		label="Play Now",
+	local function onPlayBtnRelease()		
+		composer.gotoScene( "level_selection", "fade", 500 )		
+		return true	-- indicates successful touch
+	end
+
+	local playBtn = widget.newButton{
+		label = "Play Now",
 		labelColor = { default={255}, over={128} },
-		default="button.png",
-		over="button-over.png",
-		width=154, height=40,
-		onRelease = onPlayBtnRelease	-- event listener function
+		defaultFile = "button.png",
+		overFile = "button-over.png",
+		width, height = 154, 40,
+		x = display.contentWidth/2,
+		y = display.contentHeight - 125,
+		onRelease = onPlayBtnRelease
 	}
-	playBtn.x = display.contentWidth*0.5
-	playBtn.y = display.contentHeight - 125
-	
-	-- all display objects must be inserted into group
+
 	sceneGroup:insert( background )
 	sceneGroup:insert( title )
 	sceneGroup:insert( playBtn )
 end
 
-function scene:show( event )
-	local sceneGroup = self.view
-	local phase = event.phase
-	
-	if phase == "will" then
-		-- Called when the scene is still off screen and is about to move on screen
-	elseif phase == "did" then
-		-- Called when the scene is now on screen
-		-- 
-		-- INSERT code here to make the scene come alive
-		-- e.g. start timers, begin animation, play audio, etc.
-	end	
-end
-
-function scene:hide( event )
-	local sceneGroup = self.view
-	local phase = event.phase
-	
-	if event.phase == "will" then
-		-- Called when the scene is on screen and is about to move off screen
-		--
-		-- INSERT code here to pause the scene
-		-- e.g. stop timers, stop animation, unload sounds, etc.)
-	elseif phase == "did" then
-		-- Called when the scene is now off screen
-	end	
-end
-
 function scene:destroy( event )
 	local sceneGroup = self.view
-	
-	-- Called prior to the removal of scene's "view" (sceneGroup)
-	-- 
-	-- INSERT code here to cleanup the scene
-	-- e.g. remove display objects, remove touch listeners, save state, etc.
-	
-	if playBtn then
-		playBtn:removeSelf()	-- widgets must be manually removed
-		playBtn = nil
+	for i=1, #sceneGroup do 
+		sceneGroup[i]:removeSelf()
 	end
 end
 
----------------------------------------------------------------------------------
-
--- Listener setup
 scene:addEventListener( "create", scene )
-scene:addEventListener( "show", scene )
-scene:addEventListener( "hide", scene )
 scene:addEventListener( "destroy", scene )
-
------------------------------------------------------------------------------------------
-
 return scene
