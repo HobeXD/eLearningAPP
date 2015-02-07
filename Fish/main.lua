6f5dcd62838cd29993b8f810b218b5c9077609ce
chihhsien.cheng <leisure.shadow@gmail.com>
2015-02-07 15:13:20 +0800

    add exit popup

--- Fish/main.lua
+++ Fish/main.lua
@@ -4,8 +4,23 @@ composer.gotoScene( "menu" )
 
 backscene = nil
 function onKeyEvent( event )
-	if ( event.keyName == "back" and backscene ~= nil) then
-		composer.gotoScene( backscene, "fade", 500 )
+	if ( event.keyName == "back" ) then
+		if ( backscene ~= nil ) then
+			composer.gotoScene( backscene, "fade", 500 )
+		else
+			local function onComplete( event )
+				if "clicked" == event.action then
+					local i = event.index
+					if 1 == i then
+						native.cancelAlert( alert )
+					elseif 2 == i then
+						native.requestExit()
+					end
+				end
+			end
+			local alert = native.showAlert( "EXIT", "ARE YOU SURE?", { "NO", "YES" }, onComplete )
+			-- group:insert(alert)
+		end
 		return true
 	end
 	return false
