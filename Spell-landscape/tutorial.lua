49f11877e6d6e05c2971f0d093fad3fd67eed0ff
gitqwerty777 <goohcl777@gmail.com>
2015-03-28 23:55:35 +0800

    [Refactor] tutorial.lua

--- Spell-landscape/tutorial.lua
+++ Spell-landscape/tutorial.lua
@@ -3,57 +3,33 @@ local scene = composer.newScene()
 local widget = require "widget"
 local common = require "common"
 --------------------------------------------
-local sceneGroup = display.newGroup()
-local overlayGroup = display.newGroup()
-local textGroup = display.newGroup()
+local sceneGroup = display.newGroup(); local overlayGroup = display.newGroup(); local textGroup = display.newGroup()
+local showStep -- function predeclare
 
-local stepnum
-local teachtext
-local background
-
-local backgroundOverlay1, backgroundOverlay2, backgroundOverlay3
 local welcomeText = "Touch the screen to see how to play!"
-local teachText = {"Reading", "You can see Chinese words at the top.", "Try to spell out the words in English. Each * means one alphabet.", "Type in a-n-g-r-y to collect stars!", "Listening", "You can rehear the sound by touching this button", "Try to spell out the words in English. Each * means one alphabet.", "Type in correct characters to collect stars!"}
+local teachText = {
+	"Reading", 
+	"You can see Chinese words at the top.", 
+	"Try to spell out the words in English. Each * means one alphabet.",
+	"Type in a-n-g-r-y to collect stars!",
+	"Listening",
+	"You can rehear the sound by touching this button",
+	"Try to spell out the words in English. Each * means one alphabet.",
+	"Type in correct characters to collect stars!"
+}
 
-local function showStep()
-	if stepnum > 7 then
-		backgroundOverlay3.alpha = 1
-		composer.gotoScene( "menu", "fromBottom", 500 )
-		return
-	end
-	if stepnum > 3 then -- listening
-		background.alpha = 0
-		background2.alpha = 1
-	end
-	teachtext.text = teachText[stepnum+1]
-	if stepnum%4 == 0 then
-		backgroundOverlay1.alpha = 1
-		backgroundOverlay2.alpha = 1
-		backgroundOverlay3.alpha = 1
-		teachtext.alpha = 0
-		categorytext.text = teachText[stepnum+1]
-		categorytext.alpha = 1
-	elseif stepnum%4 == 1 then
-		teachtext.alpha = 1
-		categorytext.alpha = 0
-		backgroundOverlay1.alpha = 0
-		backgroundOverlay2.alpha = 1
-		backgroundOverlay3.alpha = 1
-		teachtext.y = barH
-	elseif stepnum%4 == 2 then
-		backgroundOverlay1.alpha = 1
-		backgroundOverlay2.alpha = 0
-		backgroundOverlay3.alpha = 1
-		teachtext.y = screenTop
-	elseif stepnum%4 == 3 then
-		backgroundOverlay1.alpha = 1
-		backgroundOverlay2.alpha = 1
-		backgroundOverlay3.alpha = 0
-		teachtext.y = barH-40
-	end
-	stepnum = stepnum + 1 
+local readBackground, listenBackground
+local function createBackground()
+	readBackground = display.newImageRect( "img/full.png", display.contentWidth, display.contentHeight )
+	readBackground.x, readBackground.y = 0, 0
+	sceneGroup:insert( readBackground )
+	
+	listenBackground = display.newImageRect( "img/full_listen.png", display.contentWidth, display.contentHeight )
+	listenBackground.x, listenBackground.y = 0, 0
+	listenBackground.alpha = 0
+	sceneGroup:insert( listenBackground )
 end
-
+local backgroundOverlay1, backgroundOverlay2, backgroundOverlay3
 local function createBackgroundOverlay()
 	backgroundOverlay1 = display.newRect(overlayGroup,screenLeft, screenTop, screenW, barH)
 	backgroundOverlay1:setFillColor(0, 0, 0, 0.6)
@@ -68,7 +44,6 @@ local function createBackgroundOverlay()
 	backgroundOverlay3.isHitTestable = true 
 	backgroundOverlay3:addEventListener("tap",showStep)
 end
-
 local function createText()
 	local options = 
 	{
@@ -79,36 +54,74 @@ local function createText()
 		width = screenW,
 		font = native.systemFont,
 		fontSize = 25,
-		align = "left"  --new alignment parameter
+		align = "center"  --new alignment parameter
 	}
 	teachtext = display.newText(options)
 	
-	options.fontSize = 70
-	options.x = screencx; options.y = screency
-	options.align = "center"
-	options.text = "Reading"
-	categorytext = display.newText(options)
+	local cate_options = 
+	{
+		parent = textGroup,
+		text = "Reading",     
+		x = screencx,
+		y = screency,
+		width = screenW,
+		font = native.systemFont,
+		fontSize = 70,
+		align = "center"  --new alignment parameter
+	}
+	categorytext = display.newText(cate_options)
 	categorytext.anchorX = 0.5; categorytext.anchorY = 0.5
 end
 
-function scene:create( event )
-	--sceneGroup = self.view
-	display.setDefault( "anchorX", 0 )
-	display.setDefault( "anchorY", 0 )
+local stepnum
+local stepSectionNum = 4
+local totalStepSectionNum = stepSectionNum * 2 - 1
+showStep = function()
+	if stepnum > totalStepSectionNum then -- the end
+		backgroundOverlay3.alpha = 1
+		composer.gotoScene( "menu", "fromBottom", 500 )
+		return
+	end
+	if stepnum > stepSectionNum-1 then -- listening
+		readBackground.alpha = 0
+		listenBackground.alpha = 1
+	end
+	
+	teachtext.text = teachText[stepnum+1]
+	if stepnum % stepSectionNum == 0 then
+		backgroundOverlay1.alpha = 1
+		backgroundOverlay2.alpha = 1
+		backgroundOverlay3.alpha = 1
+		teachtext.alpha = 0
+		categorytext.text = teachText[stepnum+1]
+		categorytext.alpha = 1
+	elseif stepnum % stepSectionNum == 1 then
+		teachtext.alpha = 1
+		categorytext.alpha = 0
+		backgroundOverlay1.alpha = 0
+		backgroundOverlay2.alpha = 1
+		backgroundOverlay3.alpha = 1
+		teachtext.y = barH
+	elseif stepnum % stepSectionNum == 2 then
+		backgroundOverlay1.alpha = 1
+		backgroundOverlay2.alpha = 0
+		backgroundOverlay3.alpha = 1
+		teachtext.y = screenTop
+	elseif stepnum % stepSectionNum == 3 then
+		backgroundOverlay1.alpha = 1
+		backgroundOverlay2.alpha = 1
+		backgroundOverlay3.alpha = 0
+		teachtext.y = barH-40
+	end
+	stepnum = stepnum + 1 
+end
 
-	background = display.newImageRect( "img/full.png", display.contentWidth, display.contentHeight )
-	background.x, background.y = 0, 0
-	sceneGroup:insert( background )
-	
-	background2 = display.newImageRect( "img/full_listen.png", display.contentWidth, display.contentHeight )
-	background2.x, background2.y = 0, 0
-	background2.alpha = 0
-	sceneGroup:insert( background2 )
-	
+function scene:create( event )
+	display.setDefault( "anchorX", 0 ); display.setDefault( "anchorY", 0 )
+	createBackground()
 	createText()
 	createBackgroundOverlay()
 end
-
 function scene:show( event )
 	local phase = event.phase
 	
@@ -120,8 +133,8 @@ function scene:show( event )
 		teachtext.text = welcomeText
 		categorytext.alpha = 0
 		
-		background.alpha = 1
-		background2.alpha = 0
+		readBackground.alpha = 1
+		listenBackground.alpha = 0
 		
 	elseif phase == "did" then
 		-- Called when the scene is now on screen
@@ -130,10 +143,8 @@ function scene:show( event )
 		-- e.g. start timers, begin animation, play audio, etc.
 	end	
 end
-
 function scene:hide( event )
 	local phase = event.phase
-	
 	if event.phase == "will" then
 		sceneGroup.alpha = 0
 		overlayGroup.alpha = 0
@@ -142,7 +153,6 @@ function scene:hide( event )
 		-- Called when the scene is now off screen
 	end	
 end
-
 function scene:destroy( event )
 	display.remove(sceneGroup)
 	sceneGroup = nil
@@ -150,13 +160,8 @@ function scene:destroy( event )
 	overlayGroup = nil
 	display.remove(textGroup)
 	textGroup = nil
-	-- Called prior to the removal of scene's "view" (sceneGroup)
-	-- INSERT code here to cleanup the scene
-	-- e.g. remove display objects, remove touch listeners, save state, etc.
-	
 end
 
--- Listener setup
 scene:addEventListener( "create", scene )
 scene:addEventListener( "show", scene )
 scene:addEventListener( "hide", scene )
